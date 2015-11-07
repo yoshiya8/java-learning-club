b075f00296145d0b1d91bfa2c2adbc249a232995
yoshiya8 <jhaftel@gmail.com>
2015-11-07 09:44:40 -0500

    Imported maven artifacts for project building

---
+++ src/main/java/github/java_learning_club/season_one/App.java
@@ -0,0 +1,13 @@
+package github.java_learning_club.season_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
