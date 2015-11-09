3cf1ae961b18ae08d4313c8da3d52dec0d1b8746
yoshiya8 <jhaftel@gmail.com>
2015-11-09 16:15:39 -0500

    Project 1 and 2 stubbed

---
+++ src/main/java/github/javalearningclub/seasonone/fibonacci/Fibonacci.java
@@ -0,0 +1,19 @@
+package github.javalearningclub.seasonone.fibonacci;
+
+/**
+ * See https://github.com/yoshiya8/java-learning-club/wiki/Project-2:-Fibonacci-Sequence
+ */
+public class Fibonacci {
+	
+	public void printFirstNFibonacciNumbers(int n) {
+		// Good luck!
+	}
+	
+	public int getNthFibonacciNumber(int n) {
+		// Good luck!
+	}
+
+	public static void main(String[] args) {
+		// Call printFirstNFibonacciNumbers
+	}
+}
---
+++ src/main/java/github/javalearningclub/seasonone/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,20 @@
+package github.javalearningclub.seasonone.fizzbuzz;
+
+/**
+ * See https://github.com/yoshiya8/java-learning-club/wiki/Project-1:-Fizz-Buzz 
+ */
+public class FizzBuzz {
+	
+	public void playValue(int value) {
+		// Print value, "fizz", "buzz", or "fizz-buzz"
+	}
+	
+	public void playUpToValue(int topValue) {
+		// Loop from 1 to topValue and call "playValue" for each value
+	}
+	
+	public static void main(String args[]) {
+		// Call playUpToValue
+	}
+
+}
