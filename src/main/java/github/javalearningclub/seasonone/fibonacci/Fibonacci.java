3d5b5bbf55efc3143e28e7406dac6b3676f02e19
yoshiya8 <jhaftel@gmail.com>
2015-11-23 16:18:05 -0500

    Gabe's check-in

--- src/main/java/github/javalearningclub/seasonone/fibonacci/Fibonacci.java
+++ src/main/java/github/javalearningclub/seasonone/fibonacci/Fibonacci.java
@@ -1,27 +1,50 @@
 package github.javalearningclub.seasonone.fibonacci;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * See https://github.com/yoshiya8/java-learning-club/wiki/Project-2:-Fibonacci-
  * Sequence
  */
 public class Fibonacci {
 
-	public void printFirstNFibonacciNumbers(int n) {
-		for (int i = 0; i < n; i++) {
-			System.out.println("Fibonacci(" + i + ") = " + getNthFibonacciNumber(i));
+	private Map<Integer, Long> cache = new HashMap<>();
+
+	public void printFirstNFibonacciNumbers(long n) {
+		// loop 1-n
+		// calculate next fib #
+		// print it out
+		long last = 0;
+		long lastLast = 0;
+		long current = 1;
+		System.out.println(current);
+		for (long timeThrough = 0; timeThrough < n; timeThrough++) {
+			lastLast = last;
+			last = current;
+			current = lastLast + last;
+			System.out.println(current);
 		}
+
 	}
 
-	public int getNthFibonacciNumber(int n) {
-		int answer = 1;
-		if (n > 2) {
-			answer = getNthFibonacciNumber(n - 2) + getNthFibonacciNumber(n - 1);
+	public long getNthFibonacciNumber(int n) {
+		Long f = cache.get(n);
+		if (f != null) {
+			return f;
 		}
-		return answer;
+		if (n <= 2) {
+			f = 1L;
+		} else {
+			f = getNthFibonacciNumber(n - 1) + getNthFibonacciNumber(n - 2);
+		}
+		cache.put(n, f);
+		return f;
 	}
 
 	public static void main(String[] args) {
-		Fibonacci solver = new Fibonacci();
-		solver.printFirstNFibonacciNumbers(10);
+		Fibonacci myFibonacci = new Fibonacci();
+		System.out.println(myFibonacci.getNthFibonacciNumber(50));
 	}
 }
