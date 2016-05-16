45549618a19b7ce054fbf510db4b7bdfb50b4f0e
yoshiya8 <jhaftel@gmail.com>
2016-05-16 17:45:23 -0400

    Added PoleTest.java

--- src/test/java/edu/dolphin/towersofhanoi/model/impl/PoleTest.java
+++ src/test/java/edu/dolphin/towersofhanoi/model/impl/PoleTest.java
@@ -2,13 +2,58 @@ package edu.dolphin.towersofhanoi.model.impl;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.dolphin.towersofhanoi.model.Disk;
+import edu.dolphin.towersofhanoi.model.Pole;
+import edu.dolphin.towersofhanoi.model.TowerOfHanoiException;
+import edu.dolphin.towersofhanoi.model.impl.PoleImpl;
 
 public class PoleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Pole pole = new PoleImpl(3, Color.RED);
+		assertEquals(3, pole.getHeight());
+		assertEquals(0, pole.getDisks().size());
+		assertEquals(Color.RED, pole.getColor());
+	}
+
+	@Test
+	void testAddDiskNormal() {
+		int height = 10;
+		List<Disk> diskList = new ArrayList<>();
+		Pole pole = new PoleImpl(height, Color.RED);
+		for (int i = height; i > 0; i--) {
+			Disk disk = new DiskImpl(i, Color.GREEN);
+			diskList.add(disk);
+			pole.addDisk(disk);
+			assertEquals(diskList, pole.getDisks());
+		}
+	}
+
+	@Test(expected = TowerOfHanoiException.class)
+	void testAddTooMany() {
+		int height = 10;
+		Pole pole = new PoleImpl(height, Color.RED);
+		for (int i = height + 2; i > 0; i--) {
+			Disk disk = new DiskImpl(i, Color.GREEN);
+			pole.addDisk(disk);
+		}
+	}
+
+	@Test(expected = TowerOfHanoiException.class)
+	void testAddOutOfOrder() {
+		int height = 10;
+		Pole pole = new PoleImpl(height, Color.RED);
+		for (int i = 1; i <= height; i++) {
+			Disk disk = new DiskImpl(i, Color.GREEN);
+			pole.addDisk(disk);
+		}
 	}
 
 }
