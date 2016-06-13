05a919c4fab99b73f1da19b7655ab315a83959e4
andy abo <andrew.k.szabo@gmail.com>
2016-06-13 16:41:32 -0400

    Unfinished board

--- src/main/java/edu/dolphin/towersofhanoi/model/impl/BoardImpl.java
+++ src/main/java/edu/dolphin/towersofhanoi/model/impl/BoardImpl.java
@@ -1,35 +1,45 @@
 package edu.dolphin.towersofhanoi.model.impl;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.dolphin.towersofhanoi.model.Board;
+import edu.dolphin.towersofhanoi.model.Disk;
 import edu.dolphin.towersofhanoi.model.Pole;
 
 public class BoardImpl implements Board {
+
 	private final Color color;
-	private final Pole[] poles;
 
-	public BoardImpl(int numberOfDisks, int numberOfPole){
-		
-	}
+	private final List<Pole> poles = new ArrayList<>();
 
-	
-
-
-	{
-
+	public BoardImpl(int numberOfDisks, Color color, int numberOfPoles) {
+		this.color = color;
+		for (int poleIx = 0; poleIx < numberOfPoles; poleIx++) {
+			Pole pole = new PoleImpl(numberOfDisks, color);
+			poles.add(pole);
+			if (poleIx == 0) {
+				for (int diskSize = numberOfDisks; diskSize > 0; diskSize--){
+				Disk disk = new DiskImpl(diskSize, Color.RED);
+				pole.addDisk(disk);
+				}
+			}
+		}
 	}
 
 	@Override
 	public int getNumberOfPoles() {
 		// TODO Auto-generated method stub
-		return 0;
+		numberOf
+		return numberOfPoles;
+
 	}
 
 	@Override
 	public Pole getPole(int poleIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return poles.get(poleIndex);
+
 	}
 
 	@Override
