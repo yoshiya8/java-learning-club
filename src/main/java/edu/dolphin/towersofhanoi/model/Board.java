ed3cc1cd5d637dfd1a7f845c6b978a644dd9a087
andy abo <andrew.k.szabo@gmail.com>
2016-06-13 16:41:02 -0400

    broken BoardImpl

--- src/main/java/edu/dolphin/towersofhanoi/model/Board.java
+++ src/main/java/edu/dolphin/towersofhanoi/model/Board.java
@@ -4,4 +4,5 @@ public interface Board {
    public int getNumberOfPoles();
    public Pole getPole(int poleIndex);
    public void moveDisk(int fromPoleIndex, int toPoleIndex);
+  
 }
---
+++ src/main/java/edu/dolphin/towersofhanoi/model/impl/BoardImpl.java
@@ -0,0 +1,40 @@
+package edu.dolphin.towersofhanoi.model.impl;
+
+import java.awt.Color;
+
+import edu.dolphin.towersofhanoi.model.Board;
+import edu.dolphin.towersofhanoi.model.Pole;
+
+public class BoardImpl implements Board {
+	private final Color color;
+	private final Pole[] poles;
+
+	public BoardImpl(int numberOfDisks, int numberOfPole){
+		
+	}
+
+	
+
+
+	{
+
+	}
+
+	@Override
+	public int getNumberOfPoles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Pole getPole(int poleIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void moveDisk(int fromPoleIndex, int toPoleIndex) {
+		// TODO Auto-generated method stub
+
+	}
+}
